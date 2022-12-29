efd9667ac409400a9b3045299696d035baef6139
Rishabh Jain <51243003+rishabhjaiz@users.noreply.github.com>
2022-12-30 02:12:49 +0530

    Fix the icon

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -35,10 +35,17 @@ function Header() {
           onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
           className="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
         >
-          {mounted && resolvedTheme === "dark" ? (
-            <DarkModeSVG />
-          ) : (
-            <LightModeSVG />
+          {/** Nahi pata ye kese chala? mounted or resolved theme ek sath rakhi to phat jata he */}
+          {mounted && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              className="w-5 h-5 text-gray-800 dark:text-gray-200"
+            >
+              {resolvedTheme === "dark" ? <DarkModeSVG /> : <LightModeSVG />}
+            </svg>
           )}
         </button>
       </nav>
